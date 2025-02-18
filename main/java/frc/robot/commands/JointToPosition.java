b1a1c8f51d574f216533d7655f5a3530ea788d98
imhassett <94932727+imhassett@users.noreply.github.com>
2025-02-18 18:38:13 -0500

    Add files via upload

---
+++ main/java/frc/robot/commands/JointToPosition.java
@@ -0,0 +1,46 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Encoder;
+import frc.robot.subsystems.Joint;
+
+public class JointToPosition extends Command{
+    Joint m_joint;
+    double[] setpoints;
+    Encoder encoder;
+
+    public JointToPosition(Joint joint) {
+    m_joint = joint;
+    setpoints = m_joint.getSetpoints();
+    encoder = joint.getEncoder();
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_joint);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (encoder.isConnected()) {
+      double output = m_joint.getController().calculate(m_joint.getAngleDegrees(), m_joint.getSetpoint());
+      m_joint.setSpeed(output);
+    }
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_joint.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
